ad43ef969bb8a57e15bd35ffca5088704fec19c5
khawajasim <asimkhawaja786@gmail.com>
2023-02-17 20:51:36 +0100

    real aftershock data

--- experiment/config.py
+++ experiment/config.py
@@ -6,6 +6,7 @@ Created on Fri Feb 17 15:15:28 2023
 @author: khawajasim
 """
 
+
 #1 - Run experiment with earthquakes in negative stress
 introduce_FN = True 
 
@@ -23,8 +24,15 @@ stress_MAS= True   #True for MAS and False for OOP
 #5 - Model type to generate simulated catalogs.
 #    Set 'model_type' = 'coulomb', to use Coulomb model for catalog generation.
 #    Set 'model_type' = 'ref', to use Reference model for catalog generation. 
-model_type =  'coulomb' #'ref' 
+model_for_sim =  'coulomb' #'ref' 
 
 #6 - Number of earthquakes in the simulated catalogs to run the analysis.
 #Note: It can be any number for analysis, whether 200,300,.....2000.
 total_eqs = 400 #2485  # #
+
+#7 - Number of simulations to generate 
+num_sim = 3
+
+
+#8 - Grids to run the experiment for Chichi and Landers
+grids = ['_single_res_grid_zoom=12','_single_res_grid_zoom=13','_single_res_grid_zoom=14','_multi_res_grid'] #
